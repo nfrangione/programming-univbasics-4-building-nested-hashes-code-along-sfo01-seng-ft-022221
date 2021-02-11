618393b13d3bed2cb3c64ec44e858d99873fdd58
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-11 05:22:20 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -6,12 +6,12 @@ def third_challenge
         age: "53"
       },
       :matriarch => {
-        name: "Lady Montague"
+        name: "Lady Montague",
         age: "54"
       },
       :hero => {
-        name: "Romeo"
-        age: "15"
+        name: "Romeo",
+        age: "15",
         status: "alive"
       },
       :hero_friends => []
